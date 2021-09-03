bcdc5ec6b1ae15c5c9398b68e35f416706685aa9
RejuyanHossainShawon <76831441+RejuyanHossainShawon@users.noreply.github.com>
2021-09-03 18:31:24 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const loadData = () => {
+    const search = document.getElementById("search-input");
+    const searchText = search.value;
+    console.log(searchText);
+    // clear search field
+    search.value = "";
+    // loading  data from server by fetching
+
+}
